0fded2cd82d36fbea9e42bbc05f1f3ab6e896ad4
Pranav Ahuja <72250259+kingpranav21@users.noreply.github.com>
2025-07-24 18:59:28 +0530

    Update AnalyzeUserWebsiteVisitPattern.cpp

--- Amazon/AnalyzeUserWebsiteVisitPattern.cpp
+++ Amazon/AnalyzeUserWebsiteVisitPattern.cpp
@@ -1,3 +1,5 @@
+//AnalyzeUserWebsiteVisitPattern.cpp
+
 // In this problem, we are given the browsing history of multiple users stored in three arrays : 
 // username, timestamp, and website.Each index i in these arrays represents a single browsing event, 
 // where username[i] is the user who visited website[i] at time timestamp[i].The goal is to find a 
@@ -6,6 +8,43 @@
 // The popularity or "score" of a pattern is determined by counting how many unique users visited the 
 // websites in the same order as the pattern.The task is to return the pattern with the highest score, 
 // and in case of a tie, return the lexicographically smallest pattern.
+
+// Find the 3-sequence visited by the largest number of users. If there is more than one solution, return the lexicographically smallest such 3-sequence.
+
+ 
+
+// Example 1:
+
+// Input: username = ["joe","joe","joe","james","james","james","james","mary","mary","mary"], timestamp = [1,2,3,4,5,6,7,8,9,10], website = ["home","about","career","home","cart","maps","home","home","about","career"]
+// Output: ["home","about","career"]
+// Explanation: 
+// The tuples in this example are:
+// ["joe", 1, "home"]
+// ["joe", 2, "about"]
+// ["joe", 3, "career"]
+// ["james", 4, "home"]
+// ["james", 5, "cart"]
+// ["james", 6, "maps"]
+// ["james", 7, "home"]
+// ["mary", 8, "home"]
+// ["mary", 9, "about"]
+// ["mary", 10, "career"]
+// The 3-sequence ("home", "about", "career") was visited at least once by 2 users.
+// The 3-sequence ("home", "cart", "maps") was visited at least once by 1 user.
+// The 3-sequence ("home", "cart", "home") was visited at least once by 1 user.
+// The 3-sequence ("home", "maps", "home") was visited at least once by 1 user.
+// The 3-sequence ("cart", "maps", "home") was visited at least once by 1 user.
+ 
+
+// Note:
+
+// 3 <= N = username.length = timestamp.length = website.length <= 50
+// 1 <= username[i].length <= 10
+// 0 <= timestamp[i] <= 10^9
+// 1 <= website[i].length <= 10
+// Both username[i] and website[i] contain only lowercase characters.
+// It is guaranteed that there is at least one user who visited at least 3 websites.
+// No user visits two websites at the same time.
 
 //no of usernmaes->n
 //no of unique users->u
